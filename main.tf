e22d18d4b7762f6b036e57a674db65bfa1f4d82f
Laolu Animashaun <laolu.animashaun@iproov.com>
2025-06-03 16:12:50 +0100

    cloud build connection

--- main.tf
+++ main.tf
@@ -10,3 +10,16 @@ resource "google_artifact_registry_repository" "ai_agent_docker_image_1" {
     "environment" = var.environment
   }
 }
+
+resource "google_cloudbuildv2_connection" "my-connection" {
+  location = var.gcp_region
+  name = "ai-agent-test-connection"
+
+  github_config {
+    app_installation_id = 69466052
+
+    authorizer_credential {
+      oauth_token_secret_version = "projects/325862358211/secrets/ai-agent-syntax-errors-github-oauthtoken-c808f1/versions/1"
+    }
+  }
+}
