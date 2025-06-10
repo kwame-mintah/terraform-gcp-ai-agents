febe07dc1b8969a7b4d74ea56e6a37669020a61d
Laolu Animashaun <laolu.animashaun@iproov.com>
2025-06-10 16:08:01 +0100

    programtically create secret manager for cloud build github connection

--- main.tf
+++ main.tf
@@ -11,15 +11,51 @@ resource "google_artifact_registry_repository" "ai_agent_docker_image_1" {
   }
 }
 
-resource "google_cloudbuildv2_connection" "my-connection" {
-  location = var.gcp_region
-  name = "ai-agent-test-connection"
+# Create a secret containing the personal access token and grant permissions to the Service Agent
+resource "google_secret_manager_secret" "github_token_secret" {
+    project = var.gcp_project
+    secret_id = "syntax-errors-ai-agent-secret"
 
-  github_config {
-    app_installation_id = 69466052
-
-    authorizer_credential {
-      oauth_token_secret_version = "projects/325862358211/secrets/ai-agent-syntax-errors-github-oauthtoken-c808f1/versions/1"
+    replication {
+        auto {}
     }
-  }
+}
+
+# creates actual secret s
+resource "google_secret_manager_secret_version" "github_token_secret_version" {
+    secret = google_secret_manager_secret.github_token_secret.id
+    secret_data = "TEST_SECRET" # TODO How to safely inject secret
+}
+
+# creates data object for new iam policy
+data "google_iam_policy" "serviceagent_secretAccessor" {
+    binding {
+        role = "roles/secretmanager.secretAccessor"
+        members = ["serviceAccount:service-325862358211@gcp-sa-cloudbuild.iam.gserviceaccount.com"] # TODO Should service account be hardcoded
+        #members = ["serviceAccount:laolu-terraform-development@syntax-errors.iam.gserviceaccount.com"] # TODO Should service account be hardcoded
+
+    }
+}
+
+# creates actual iam policy giving service account access to secret
+resource "google_secret_manager_secret_iam_policy" "policy" {
+  project = google_secret_manager_secret.github_token_secret.project
+  secret_id = google_secret_manager_secret.github_token_secret.secret_id
+  policy_data = data.google_iam_policy.serviceagent_secretAccessor.policy_data
+}
+
+# creates connection from cloudbuild to github
+// Create the GitHub connection
+resource "google_cloudbuildv2_connection" "my_connection" {
+    project = var.gcp_project
+    location = var.gcp_region
+    name = "ai-agent-test-connection"
+
+    github_config {
+        app_installation_id = 69466052
+        authorizer_credential {
+            oauth_token_secret_version = google_secret_manager_secret_version.github_token_secret_version.id
+        }
+    }
+    depends_on = [google_secret_manager_secret_iam_policy.policy]
 }
