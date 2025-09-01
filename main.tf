4134d7a75565ad272b5609ea31dd19b944e366d4
Laolu Animashaun <laolu.animashaun@iproov.com>
2025-09-01 23:19:28 +0100

    terragrunt plan completes

--- main.tf
+++ main.tf
@@ -136,11 +136,6 @@ resource "google_container_cluster" "default" {
    network    = data.google_compute_network.default.id
    subnetwork = data.google_compute_subnetwork.default.id
 
-  ip_allocation_policy {
-     stack_type                    = "IPV4_IPV6"
-     services_secondary_range_name = google_compute_subnetwork.default.secondary_ip_range[0].range_name
-     cluster_secondary_range_name  = google_compute_subnetwork.default.secondary_ip_range[1].range_name
-  }
 
    # Set `deletion_protection` to `true` will ensure that one cannot
    # accidentally delete this instance by use of Terraform.
