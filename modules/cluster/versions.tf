bafa07e964e20e90dd1a20643f9c969fcde5baf7
Laolu Animashaun <la.olu@live.co.uk>
2025-10-13 22:56:54 +0100

    Cluster module (#12)
    * wips
    
    * fix load balancer
    
    * wips
    
    * use module
    
    * seperate gke cluster from kubernetes
    
    * fix bug where docker image was not found

---
+++ modules/cluster/versions.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.5.7, <= 1.9.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.6.0"
+    }
+
+    sops = {
+      source  = "carlpett/sops"
+      version = "~> 1.2.0"
+    }
+
+    kubernetes = {
+        source = "hashicorp/kubernetes"
+        version = "~> 2.38.0"
+    }
+  }
+}
